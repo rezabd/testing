b65127ca2549385261cfb208119341b801733232
unknown <rkk_bd@yahoo.com>
2017-04-23 18:10:36 +0600

    initial commit

---
+++ src/test/MainClass.java
@@ -0,0 +1,34 @@
+package test;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		//int a = 6;
+		int a = input.nextInt();
+/*		int b = input.nextInt();
+		int c = input.nextInt();
+*/		
+		switch (a) {
+			case 1:
+				System.out.println("value of a="+a);
+				break;
+			case 2:
+				System.out.println("value of a="+a);
+				break;
+
+			case 3:
+				System.out.println("value of a="+a);
+				break;
+
+
+			default:
+				System.out.println("value of a="+a);
+				break;
+			}
+		
+
+}}
